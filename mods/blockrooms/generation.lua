97313975f3ce38a9f64ffdb38c602ddefa61fbbf
benjaminpants <maltbyfamily@gmail.com>
2022-05-14 04:00:22 -0700

    remove debug messages

--- mods/blockrooms/generation.lua
+++ mods/blockrooms/generation.lua
@@ -4,10 +4,10 @@ local c_air = minetest.get_content_id("air")
 
 minetest.register_on_generated(function(minp, maxp, seed)
 	for i in pairs(blockrooms.floors.levels) do
-		minetest.chat_send_all(i)
+		--minetest.chat_send_all(i)
 		local v = blockrooms.floors.levels[i]
 		for y=1, v.layers_to_allocate do
-			minetest.chat_send_all("text:" .. y)
+			--minetest.chat_send_all("text:" .. y)
 			if (minp.y == blockrooms.get_start_floor_y((v.starting_y - 1) + y)) then
 				v.generator(minp,maxp,seed, y)
 			end
